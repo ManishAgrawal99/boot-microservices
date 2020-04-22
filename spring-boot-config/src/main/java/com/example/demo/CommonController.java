1b02a6828c2d67d3156416c54f5cad1769832071
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-04-22 13:36:59 +0530

    Populated properties file

--- spring-boot-config/src/main/java/com/example/demo/CommonController.java
+++ spring-boot-config/src/main/java/com/example/demo/CommonController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CommonController {
 
-	@Value("${my.greeting}")
+	@Value("${my.greeting : default value}")
 	public String greetingMessage;
+	
+	@Value("#{${dbvalues}}")
+	private Map<String, String> dbvals;
 	
 	
 	@GetMapping("/greeting")
 	public String greeting() {
-		return greetingMessage;
+		return greetingMessage + dbvals;
 	}
 }
