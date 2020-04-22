62a01637844432b76cad3916bcb307d6db0cb272
ManishAgrawal99 <manish.agrawal1999@gmail.com>
2020-04-22 13:54:44 +0530

    Making use of @ConfigurationProperties

--- spring-boot-config/src/main/java/com/example/demo/CommonController.java
+++ spring-boot-config/src/main/java/com/example/demo/CommonController.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +10,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CommonController {
 
-	@Value("${my.greeting : default value}")
+	@Value("${my.greeting: default value}")
 	public String greetingMessage;
 	
-	@Value("#{${dbvalues}}")
-	private Map<String, String> dbvals;
+//	@Value("#{${dbvalues}}")
+//	private Map<String, String> dbvals;
+	
+	
+	@Autowired
+	private DbSettings dbSettings;
 	
 	
 	@GetMapping("/greeting")
 	public String greeting() {
-		return greetingMessage + dbvals;
+		return greetingMessage + " " + dbSettings.getConnection() + " " + dbSettings.getHost()+":" + dbSettings.getPort();
 	}
 }
